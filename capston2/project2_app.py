a11aeccc45490dc35eab80c99a7202cc0547a5ac
WaadAlharthi <66639713+WaadAlharthi@users.noreply.github.com>
2023-12-06 11:03:52 +0300

    Add files via upload
    Updated interface

--- capston2/project2_app.py
+++ capston2/project2_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 import pickle
 import pandas as pd
 from sklearn.compose import make_column_transformer
@@ -11,16 +12,15 @@ html_temp = """
 </div>"""
 st.markdown(html_temp, unsafe_allow_html=True)
 
-avg_monthly_hours=st.sidebar.slider("What is your average monthly hours", 40, 400, step=1)
-satisfaction_level=st.sidebar.slider('How much is your satisfaction level?',0, 100, step=1)
-last_evaluation=st.sidebar.slider("What was your last evaluation?", 0, 100, step=1)
-num_project=st.sidebar.slider("Select the number of projects", 1,10, step=1)
-time_in_company=st.sidebar.slider("How many years did you spend in the company?",1,20, step=1)
-salary=st.sidebar.selectbox("Which category describes you salary?", ('Low', 'Medium', 'High'))
-department=st.sidebar.selectbox("Which department do you work in?", ('Sales', 'Technical', 'Support','IT', 'RandD', 'Product manager','Marketing', 'Accounting', 'HR', 'Management'))
+avg_monthly_hours=st.sidebar.slider("What is the employee average monthly hours", 40, 400, step=1)
+satisfaction_level=st.sidebar.slider('How much is the employee satisfaction level?',0, 100, step=1)
+last_evaluation=st.sidebar.slider("How much was the employee last evaluation?", 0, 100, step=1)
+num_project=st.sidebar.slider("How many project is the employee working on", 1,10, step=1)
+time_in_company=st.sidebar.slider("How many years did the employee spend in the company?",1,20, step=1)
+salary=st.sidebar.selectbox("Which category describes the employee salary?", ('Low', 'Medium', 'High'))
+department=st.sidebar.selectbox("Which department is the employee working in?", ('Sales', 'Technical', 'Support','IT', 'RandD', 'Product manager','Marketing', 'Accounting', 'HR', 'Management'))
 
-rf_model=pickle.load(open("model.pkl","rb"))
-transformer = pickle.load(open('transformer.pkl', 'rb'))
+rf_model=pickle.load(open("rf2_model","rb"))
 
 
 my_dict = {
@@ -30,7 +30,7 @@ my_dict = {
     "number_project": num_project, 
     "time_spend_company": time_in_company,
     "salary": salary, 
-    "Departments": department
+    "Departments ": department
 }
 
 df = pd.DataFrame.from_dict([my_dict])
@@ -39,11 +39,15 @@ df = pd.DataFrame.from_dict([my_dict])
 st.header("Your information as you selected is below")
 st.table(df)
 
-df2 = transformer.transform(df)
-
-
 st.subheader("Press predict when you're ready")
 
 if st.button("Predict"):
-    prediction = rf_model.predict(df2)
-    st.success("The likelyhood of you leaving the company is: {}. ".format(int(prediction[0])))
+    prediction = rf_model.predict(df)
+    if prediction == 1:
+        st.success("This employee will most likely leave the company soon")
+    else:
+        st.success("This employee is unlikely to leave the company soon")
+
+
+
+st.lottie("https://lottie.host/649b9bb5-1058-4b31-82fe-7b7a0f091e15/PmAZRKaK7z.json")
